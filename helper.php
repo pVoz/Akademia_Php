3c37c0ef02b288f609dbf69b59ff10cea5c6b306
Peter Voz <vozarp93@gmail.com>
2023-01-12 14:59:12 +0100

    pridal som times.json ktoré priradí id a čas a zapíš do súboru

--- helper.php
+++ helper.php
@@ -60,23 +60,30 @@ function validateName($name)
 
 function fileSave($stime, $time, $name)
 {
+    $time = $time->format('d.m.Y H:i:s');
     $startSchool = strtotime("08:00:00");
     $message = compareTime($stime, $startSchool, '<h2>Študent meškal !!</h2>');
     $myfile = fopen("Date-time.dat", "a+") or die("Unable to open file!");
-    fwrite($myfile, $name . "  prišiel o  " . $time->format('d.m.Y H:i:s') . " " . $message  . PHP_EOL);
-    
+    fwrite($myfile, $name . "  prišiel o  " . $time . " " . $message  . PHP_EOL);
+
     fclose($myfile);
 };
 
+function getTime($dtime)
+{
+    echo $dtime;
+};
+echo getTime($dtime);
 function fileGet()
 {
-    
+
     $file = file_get_contents("Date-time.dat", true);
     echo ('<pre>') . $file . ('</pre>');
 };
 ////////////////////////////////////////////////////////
 function pre_r($data)
 {
+    
     echo '<pre>';
     print_r($data);
     echo '<pre>';
@@ -87,32 +94,34 @@ $students = file_get_contents('students.json');
 
 function appendStudent($studends_json, $name)
 {
-    
+
     $students_decoded = json_decode($studends_json, true);
-    
+
     //create new id
     $new_id  = count($students_decoded) + 1;
-    
+
     //create new student
     $new_student = [
         'id' => $new_id,
         'name' => $name
-        
+
     ];
-    
+
     //append new studend
     $students_decoded[] = $new_student;
-    
+
     $students_encoded = json_encode($students_decoded, JSON_PRETTY_PRINT);
-    
+
     file_put_contents('students.json', $students_encoded);
 }
 
+
+
 function showStudents($studends_json)
 {
-    
+
     $students_decoded = json_decode($studends_json, true);
-    
+
     pre_r($students_decoded);
 }
 if (isset($_POST) && !empty($_POST)) {
@@ -121,4 +130,43 @@ if (isset($_POST) && !empty($_POST)) {
 
 $students = file_get_contents('students.json');
 showStudents($students);
+/////////////////////////////
+/////////////////////////////
+/////////////////////////////
+function saveTime($id, $time) {
+    $times = json_decode(file_get_contents('times.json'), true);
+    $new_time = [
+        'id' => $id,
+        'time' => $time
+    ];
+    $times[] = $new_time;
+    $times_json = json_encode($times, JSON_PRETTY_PRINT);
+    file_put_contents('times.json', $times_json);
+}
+
+
+///
+$students = json_decode(file_get_contents('students.json'), true);
+foreach($students as $student) {
+    if ($student['name'] === $name) {
+        $student_id = $student['id'];
+        break;
+    }
+}
+/////////
+saveTime($student_id, $dtime);
+////////////////////
+/////////////////////////////
+/////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
 
